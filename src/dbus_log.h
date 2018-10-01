ac935565b18d6b92fee695c4f8e4e063f73cdb58
Mike Crowe <mcrowe@brightsign.biz>
2018-10-01 09:28:00 +0100

    dbus-asio: Add licence and headers to source files BS#31645
    We are releasing this code under LGPLv3+.  Let's make that explicit.
    
    Change-Id: Ib9336d5ca9f63816ba6cdb03f03afe19c1e24467

--- src/dbus_log.h
+++ src/dbus_log.h
@@ -1,3 +1,20 @@
+// This file is part of dbus-asio
+// Copyright 2018 Brightsign LLC
+//
+// This library is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, version 3, or at your
+// option any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// The GNU Lesser General Public License version 3 is included in the
+// file named COPYING. If you do not have this file see
+// <http://www.gnu.org/licenses/>.
+
 #ifndef DBUS_LOG_H
 #define DBUS_LOG_H
 
