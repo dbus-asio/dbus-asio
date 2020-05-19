d2ff5f4ff9020e5dc5d45af199af3e65b75336c7
A Developer <nobody@brightsign.biz>
2020-05-19 10:51:49 +0100

    dbus-asio: Remove validation code
    The validation code was slow, using regex and incomplete.
    It accounted for ~20% of the time when running tests. Let's remove
    almost all of it. The check for the key type of a dictionary entry is
    cheap, so we can keep that.
    
    Change-Id: I7d496c94cc4a135ab16594ac225c1b4fcb4beaa1

--- src/dbus_validation.h
+++ src/dbus_validation.h
@@ -1,5 +1,5 @@
 // This file is part of dbus-asio
-// Copyright 2018 Brightsign LLC
+// Copyright 2018-2020 Brightsign LLC
 //
 // This library is free software: you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public License
@@ -18,34 +18,15 @@
 #ifndef DBUS_VALIDATION_H
 #define DBUS_VALIDATION_H
 
+#include <string>
+#include <stdexcept>
+
 namespace DBus {
-namespace Validation {
-    void throwOnInvalidRule(const std::string& rule);
-
-    void throwOnInvalidBoolean(uint32_t value);
-    void throwOnInvalidSignature(const std::string& signature);
-    void throwOnInvalidObjectPath(const std::string& path);
-    void throwOnInvalidBasicType(const std::string& type);
-    void throwOnInvalidSingleCompleteType(const std::string& type);
-    void throwOnInvalidContainerType(const std::string& type);
-    void throwOnInvalidInterfaceName(const std::string& name);
-    void throwOnInvalidBusName(const std::string& name);
-    void throwOnInvalidMethodName(const std::string& name);
-    void throwOnInvalidErrorName(const std::string& name);
-
-    bool isValidRule(const std::string& rule);
-    bool isValidFixedType(const std::string& type);
-    bool isValidStringLikeType(const std::string& type);
-    bool isValidSignature(const std::string& signature);
-    bool isValidObjectPath(const std::string& path);
-    bool isValidBasicType(const std::string& type);
-    bool isValidSingleCompleteType(const std::string& type);
-    bool isValidContainerType(const std::string& type);
-    bool isValidInterfaceName(const std::string& name);
-    bool isValidBusName(const std::string& name);
-    bool isValidMethodName(const std::string& name);
-    bool isValidErrorName(const std::string& name);
-}
+    namespace Validation {
+        bool isValidBasicType(char type);
+        void throwOnInvalidBasicType(char type);
+        void throwOnInvalidBasicType(const std::string &type);
+    }
 }
 
 #endif // DBUS_VALIDATION_H
