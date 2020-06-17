48cc05725de608bb1aaa78084e382dd1a9d2bf97
Mike Crowe <mcrowe@brightsign.biz>
2020-06-17 17:22:22 +0100

    tests: Add tests for strange and truncated strings
    The DBus specification does not permit embedded NUL in strings but it
    does permit anything else. Let's try some control characters including
    newlines and some bad UTF-8 just to prove that we don't object to it.
    
    Also, let's try truncating the input stream at various places to try and
    provoke problems that valgrind would detect. It didn't.
    
    Change-Id: Ie85982c3d184330c79b4829325991c2a75cb81e9

--- tests/dbus_type_string_unittest.cpp
+++ tests/dbus_type_string_unittest.cpp
@@ -20,7 +20,8 @@ void TestUnmarshallFromMessageIStream(const std::string &stream,
     REQUIRE(dbusString.asString() == str);
 }
 
-void TestUnmarshallFromMessageIStream(unsigned byteOrder, const std::string& str)
+void TestUnmarshallFromMessageIStream(unsigned byteOrder, const std::string& str,
+                                      std::string::size_type truncate_at = std::string::npos)
 {
     std::string stream;
     uint32_t length = str.size();
@@ -33,17 +34,87 @@ void TestUnmarshallFromMessageIStream(unsigned byteOrder, const std::string& str
     stream.append(str);
     stream.push_back('\0');
 
+    if (stream.size() > truncate_at)
+        stream.erase(truncate_at);
+
+    fprintf(stderr, "%u\n", stream.size());
+
     TestUnmarshallFromMessageIStream(stream, byteOrder, str);
 }
 
-TEST_CASE("Unmarshall string - little endian from MessageIStream")
+void TestUnmarshallFromMessageIStream(const std::string &str)
 {
-    TestUnmarshallFromMessageIStream(__LITTLE_ENDIAN, "This is a string");
+    SECTION("Little endian")
+    {
+        TestUnmarshallFromMessageIStream(__LITTLE_ENDIAN, "This is a string");
+    }
+
+    SECTION("Big endian")
+    {
+        TestUnmarshallFromMessageIStream(__BIG_ENDIAN, "This is a string");
+    }
 }
 
-TEST_CASE("Unmarshall string - big endian from MessageIStream")
+TEST_CASE("Unmarshall string")
 {
-    TestUnmarshallFromMessageIStream(__BIG_ENDIAN, "This is another string");
+    TestUnmarshallFromMessageIStream("This is a string");
+}
+
+TEST_CASE("Unmarshall string with control characters - from MessageIStream")
+{
+    TestUnmarshallFromMessageIStream("Control characters \r\n\n\r\b \xc2\xa3 \xf0\x9f\x92\xa9");
+}
+
+TEST_CASE("Unmarshall string with bad UTF-8")
+{
+    // 0xc2 is a lead octet for a two-octet UTF-8 character.
+    // 0xf0 is a lead octet for a four-octet UTF-8 character.
+    TestUnmarshallFromMessageIStream("Not UTF-8 \xc2. 0\xf0");
+}
+
+TEST_CASE("Truncated stream")
+{
+    for (const auto endian : { __LITTLE_ENDIAN, __BIG_ENDIAN })
+    {
+        INFO(((endian == __LITTLE_ENDIAN) ? "Little endian" : "Big endian"));
+
+        const std::string test_string{"To be truncated"};
+        SECTION("Remove terminator")
+        {
+            REQUIRE_THROWS_WITH(TestUnmarshallFromMessageIStream(__LITTLE_ENDIAN, "Not UTF-8 \xc2. 0\xf0", 19),
+                                "OctetBuffer index out of range error");
+        }
+        SECTION("Remove some of string")
+        {
+            REQUIRE_THROWS_WITH(TestUnmarshallFromMessageIStream(__LITTLE_ENDIAN, "Not UTF-8 \xc2. 0\xf0", 16),
+                                "Read string error: Not enough data in stream");
+        }
+        SECTION("Remove string body")
+        {
+            REQUIRE_THROWS_WITH(TestUnmarshallFromMessageIStream(__LITTLE_ENDIAN, "Not UTF-8 \xc2. 0\xf0", 4),
+                                "Read string error: Not enough data in stream");
+        }
+        SECTION("Remove some of length 3")
+        {
+            REQUIRE_THROWS_WITH(TestUnmarshallFromMessageIStream(__LITTLE_ENDIAN, "Not UTF-8 \xc2. 0\xf0", 3),
+                                "OctetBuffer copy error: Not enough data in buffer");
+        }
+        SECTION("Remove some of length 2")
+        {
+            REQUIRE_THROWS_WITH(TestUnmarshallFromMessageIStream(__LITTLE_ENDIAN, "Not UTF-8 \xc2. 0\xf0", 2),
+                                "OctetBuffer copy error: Not enough data in buffer");
+        }
+        SECTION("Remove some of length 1")
+        {
+            REQUIRE_THROWS_WITH(TestUnmarshallFromMessageIStream(__LITTLE_ENDIAN, "Not UTF-8 \xc2. 0\xf0", 1),
+                                "OctetBuffer copy error: Not enough data in buffer");
+        }
+        SECTION("Remove everything")
+        {
+            REQUIRE_THROWS_WITH(TestUnmarshallFromMessageIStream(__LITTLE_ENDIAN, "Not UTF-8 \xc2. 0\xf0", 0),
+                                "OctetBuffer copy error: Not enough data in buffer");
+        }
+    }
 }
 
 TEST_CASE("Marshall and unmarshall string")
