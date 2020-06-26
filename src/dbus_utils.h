349910465078a26cc138e067146e1732be888a7f
Mike Crowe <mcrowe@brightsign.biz>
2020-06-26 15:51:09 +0100

    dbus_utils: Add missing <string> include
    dbus_utils.h should not rely on someone else having included <string>.
    
    Change-Id: I5c91fd2bebcee276d456e0d44c32b560a6cd44f5

--- src/dbus_utils.h
+++ src/dbus_utils.h
@@ -18,6 +18,8 @@
 #ifndef DBUS_UTILS_H
 #define DBUS_UTILS_H
 
+#include <string>
+
 namespace DBus {
 namespace Utils {
     void ConvertHexStringToBinary(std::string& result, const std::string& input);
