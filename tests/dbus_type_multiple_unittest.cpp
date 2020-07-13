66f2177aac441109349b07b98a2ee1ace0bb9f67
Mike Crowe <mcrowe@brightsign.biz>
2020-07-13 21:28:28 +0100

    tests: Avoid using non-C++14 namespace feature
    GCC let us get away with specifying multiple namespaces even though
    it's a C++17 feature. Clang doesn't, so let's not do that. Unfortunately
    this means that clang-format needs to indent the entire file too.

--- tests/dbus_type_multiple_unittest.cpp
+++ tests/dbus_type_multiple_unittest.cpp
@@ -14,38 +14,39 @@
 #include <iomanip>
 #include <iostream>
 
-namespace DBus::test {
+namespace DBus {
+namespace test {
 
-// This only tests that we can receive what we send. It doesn't prove
-// that we'll interoperate with any other implementations.
-TEST_CASE("Round trip complex data structure")
-{
-    Type::Struct dbusStruct1;
-    dbusStruct1.add(Type::Byte(85));
-    dbusStruct1.add(Type::String("BrightSign"));
+    // This only tests that we can receive what we send. It doesn't prove
+    // that we'll interoperate with any other implementations.
+    TEST_CASE("Round trip complex data structure")
+    {
+        Type::Struct dbusStruct1;
+        dbusStruct1.add(Type::Byte(85));
+        dbusStruct1.add(Type::String("BrightSign"));
 
-    Type::Struct dbusStruct2;
-    dbusStruct2.add(Type::Byte(42));
-    dbusStruct2.add(Type::String("Arthur"));
+        Type::Struct dbusStruct2;
+        dbusStruct2.add(Type::Byte(42));
+        dbusStruct2.add(Type::String("Arthur"));
 
-    Type::Struct dbusStruct3;
-    dbusStruct3.add(Type::Byte(99));
-    dbusStruct3.add(Type::String("Red Balloons"));
+        Type::Struct dbusStruct3;
+        dbusStruct3.add(Type::Byte(99));
+        dbusStruct3.add(Type::String("Red Balloons"));
 
-    Type::Array dbusArray;
-    dbusArray.add(Type::DictEntry(Type::String("KeyKey"), dbusStruct1));
-    dbusArray.add(Type::DictEntry(Type::String("Another key"), dbusStruct2));
-    dbusArray.add(Type::DictEntry(Type::String("Anarchy"), dbusStruct3));
+        Type::Array dbusArray;
+        dbusArray.add(Type::DictEntry(Type::String("KeyKey"), dbusStruct1));
+        dbusArray.add(Type::DictEntry(Type::String("Another key"), dbusStruct2));
+        dbusArray.add(Type::DictEntry(Type::String("Anarchy"), dbusStruct3));
 
-    Type::Struct dbusStructTop;
-    dbusStructTop.add(Type::String("This is a long string value"));
-    dbusStructTop.add(Type::DictEntry(Type::String("Key"), dbusArray));
+        Type::Struct dbusStructTop;
+        dbusStructTop.add(Type::String("This is a long string value"));
+        dbusStructTop.add(Type::DictEntry(Type::String("Key"), dbusArray));
 
-    Type::DictEntry input(Type::Int32(131073), dbusStructTop);
+        Type::DictEntry input(Type::Int32(131073), dbusStructTop);
 
-    REQUIRE(DBus::Type::getMarshallingSignature(input) == "{i(s{sa{s(ys)}})}");
-    REQUIRE('\n' + input.toString() ==
-        R"(
+        REQUIRE(DBus::Type::getMarshallingSignature(input) == "{i(s{sa{s(ys)}})}");
+        REQUIRE('\n' + input.toString() ==
+            R"(
 DictEntry ({i(s{sa{s(ys)}})}) : {
    key:      Int32 131073 (0x20001)
    value:    Struct (s{sa{s(ys)}}) <
@@ -83,40 +84,41 @@ DictEntry ({i(s{sa{s(ys)}})}) : {
 }
 )");
 
-    MessageOStream ostream;
-    input.marshall(ostream);
+        MessageOStream ostream;
+        input.marshall(ostream);
 
-    REQUIRE(ostream.data.size() == 165);
+        REQUIRE(ostream.data.size() == 165);
 
-    SECTION("Complete")
-    {
-        Type::DictEntry output;
-        output.setSignature(input.getSignature());
-        MessageIStream istream((uint8_t*)ostream.data.data(), ostream.data.size(),
-            false);
-        output.unmarshall(istream);
+        SECTION("Complete")
+        {
+            Type::DictEntry output;
+            output.setSignature(input.getSignature());
+            MessageIStream istream((uint8_t*)ostream.data.data(), ostream.data.size(),
+                false);
+            output.unmarshall(istream);
 
-        REQUIRE(input.toString() == output.toString());
-    }
+            REQUIRE(input.toString() == output.toString());
+        }
 
-    SECTION("Truncated input")
-    {
-        // We don't really care what exception is thrown as long as
-        // one is and valgrind doesn't complain that we went outside
-        // the bounds.
-        for (const auto truncate_at : { 164, 42, 1, 0 }) {
-            SECTION("Truncated")
-            {
-                INFO("Truncate at " << truncate_at << " from " << ostream.data.size());
-                std::string data = ostream.data.substr(0, truncate_at);
-                Type::DictEntry output;
-                output.setSignature(input.getSignature());
+        SECTION("Truncated input")
+        {
+            // We don't really care what exception is thrown as long as
+            // one is and valgrind doesn't complain that we went outside
+            // the bounds.
+            for (const auto truncate_at : { 164, 42, 1, 0 }) {
+                SECTION("Truncated")
+                {
+                    INFO("Truncate at " << truncate_at << " from " << ostream.data.size());
+                    std::string data = ostream.data.substr(0, truncate_at);
+                    Type::DictEntry output;
+                    output.setSignature(input.getSignature());
 
-                MessageIStream istream((uint8_t*)data.data(), data.size(), false);
-                REQUIRE_THROWS(output.unmarshall(istream));
+                    MessageIStream istream((uint8_t*)data.data(), data.size(), false);
+                    REQUIRE_THROWS(output.unmarshall(istream));
+                }
             }
         }
     }
-}
 
-} // namespace DBus::test
+} // namespace test
+} // namespace DBus
