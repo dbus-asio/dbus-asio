dd44b948bd235bcbd972133d7c072544defb69d4
Mike Crowe <mcrowe@brightsign.biz>
2020-07-13 17:26:59 +0100

    tests: Reformat comment to avoid warning
    It appears that clang-format split a comment in test_main.cpp in such a
    way that GCC warns about it:
    
     ../tests/test_main.cpp:1:27: error: multi-line comment [-Werror=comment]
      #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this \
    
    Let's move the comment to the previous line to avoid that.

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,3 +1,3 @@
-#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this \
-    // in one cpp file
+// This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
